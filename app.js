ff14508b1b911c9905e67f73f583dc63649f9165
Elisabeth Edrington <edringtondc@gmail.com>
2020-06-06 14:16:52 -0600

    add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
-require("./dotenv").config();
+require("dotenv").config();
 
 const https = require('https');
 let response;
